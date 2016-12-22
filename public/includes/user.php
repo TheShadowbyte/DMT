c50de89d8d59889c8b0a8af0c3fcfb5a14ee112b
Dimitar Jordanov <theshadowbyte@gmail.com>
2016-12-22 16:22:28 -0500

    Password Hashing
    Added method to hash user password with Blowfish on registration.

--- public/includes/user.php
+++ public/includes/user.php
@@ -17,23 +17,34 @@
 			$this->passwordConfirm = $_POST['password-confirm'];
 			$this->postType = $_POST['post-type'];
 			$this->database = new Database();
+			$this->encryptedPassword = $this->passwordEncrypt();
 		}
 
 		// Perform user registration
 		public function register() {
-			if ($this->registerCheckExistingUsername() == false) {
-				$sql = "INSERT INTO users (username, password, email, user_type) VALUES ('$this->username', '$this->password', '$this->email', 'registered')";
+			if ($this->usernameExistent() == false && $this->emailExistent() == false) {
+				$sql = "INSERT INTO users 
+										(
+											username,
+											password,
+											email,
+											user_type
+										) 
+								VALUES (
+											'$this->username', 
+											'$this->encryptedPassword', 
+											'$this->email', 
+											'registered'
+										)";
 				return $this->database->query($sql);
 			}
 			else {
-				// Don't register if username exists
-				// In the future create 'user already exists' notification to client.
 				return false;
 			}
 		}
 
 		// Returns true if username already exists
-		private function registerCheckExistingUsername() {
+		private function usernameExistent() {
 			$sql = "SELECT * FROM users WHERE username='$this->username' LIMIT 1";
 			if (mysqli_num_rows($this->database->query($sql)) > 0) {
 				return true;
@@ -41,6 +52,25 @@
 			else {
 				return false;
 			}
+		}
+
+		// Returns true if email address is already in use
+		private function emailExistent() {
+			$sql = "SELECT * FROM users WHERE email='$this->email' LIMIT 1";
+			if (mysqli_num_rows($this->database->query($sql)) > 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		// Encrypts password using Blowfish
+		private function passwordEncrypt() {
+			if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+        		$salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+        		return crypt($this->password, $salt);
+    		}
 		}
 
 	}
